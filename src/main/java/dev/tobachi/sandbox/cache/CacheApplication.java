73a1fa5213358ba48e9214739c2c711db1575ca8
keisuke.teraoka <keisuke.teraoka@asoview.co.jp>
2020-05-08 20:12:06 +0900

    first commit

---
+++ src/main/java/dev/tobachi/sandbox/cache/CacheApplication.java
@@ -0,0 +1,13 @@
+package dev.tobachi.sandbox.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheApplication.class, args);
+	}
+
+}
